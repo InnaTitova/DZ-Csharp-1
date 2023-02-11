8ac584b372dbef4d75d61dc5626d090052d48c82
Inna_IT <leto2077@gmail.com>
2023-02-12 00:46:06 +0300

    add reshenie

--- DZ-Csh-Zad1/Program.cs
+++ DZ-Csh-Zad1/Program.cs
@@ -3,3 +3,15 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+System.Console.Write("Введите число 1: ");
+int number1= Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число 2: ");
+int number2= Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{
+    System.Console.WriteLine($"Максимальное число =  {number1}");
+}
+else
+{
+    System.Console.WriteLine($"Максимальное число =  {number2}");
+}
